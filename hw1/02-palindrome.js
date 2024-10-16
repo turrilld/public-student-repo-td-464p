ddebd0eb0b65a6595472e48beb4aa9cf2b60c516
turrilld <turrilld@pdx.edu>
2024-10-15 21:53:19 -0700

    Palindrome homework!

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,36 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
+elem.addEventListener("input", handleInput);
+handleInput();
 
-elem.addEventListener('input', handleInput);
+const isPalindrome = (function () {
+  if (elem.value < 0) return false;
+  return true;
+})();
+
+function handleInput() {
+  const isPalindrome = (function () {
+    if (elem.value < 0) return false;
+
+    const numbers = elem.value.split("");
+    const numbersReversed = numbers.reverse();
+    const elemReversed = numbersReversed.join("");
+
+    if (elem.value != elemReversed) return false;
+
+    return true;
+  })();
+
+  if (elem.value < 0) {
+    yes.hidden = true;
+    no.hidden = true;
+    error.hidden = false;
+  } else if (isPalindrome == true) {
+    yes.hidden = false;
+    no.hidden = true;
+    error.hidden = true;
+  } else {
+    yes.hidden = true;
+    no.hidden = false;
+    error.hidden = true;
+  }
+}
