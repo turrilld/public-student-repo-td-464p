2562cfd648fcdfe6662a268aae89286ae21de30f
turrilld <turrilld@pdx.edu>
2024-10-28 15:22:50 -0700

    Part 3: backend now functional

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,21 @@
 // Add your code here
+const searchableText = document.getElementById("text-to-search");
+const input = document.getElementById("userInput");
+input.addEventListener("keyup", handleKeyDown);
 
-input.addEventListener('keydown', handleKeyDown);
+function handleKeyDown() {
+  let text = searchableText.textContent || searchableText.innerText || "";
+  searchableText.innerHTML = text;
+
+  let wordToSeachFor = input.value.trim();
+  let textToSearch = searchableText.innerHTML;
+  let searchResult = textToSearch.search(wordToSeachFor);
+
+  if (searchResult >= 0 && wordToSeachFor !== "") {
+    let newText = textToSearch.replace(
+      wordToSeachFor,
+      `<mark style="background-color: yellow">${wordToSeachFor}</mark>`
+    );
+    searchableText.innerHTML = newText;
+  }
+}
