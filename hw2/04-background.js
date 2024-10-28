109d55d81f926682f8f6f42e58df0ac63391c993
turrilld <turrilld@pdx.edu>
2024-10-28 15:50:05 -0700

    Part 4 styling done, start of backend

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,5 @@
 // Add your code here
+
+const body = document.querySelector("body");
+const intervalSeconds = document.getElementById("interval");
+const startStopButton = document.getElementById("start-stop-button");
