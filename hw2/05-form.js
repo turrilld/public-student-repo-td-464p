32512be0a7e6519d91aa5b303bbf3dc24e64a6ad
turrilld <turrilld@pdx.edu>
2024-10-28 20:33:52 -0700

    Part 5, Modal now displaying.

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,17 @@
 // Add your code here
+const form = document.getElementById("form");
+form.addEventListener("click", submit);
+
+const modalDump = document.getElementById("modal-dump");
+const modalClose = document.getElementById("modal-close");
+modalClose.addEventListener("click", close);
+
+function submit() {
+  //   $("#modal").modal("show");
+  //   window.alert("ran");
+  modalDump.style.display = "block";
+}
+
+function close() {
+  modalDump.style.display = "none";
+}
