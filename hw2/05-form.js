4744d9460f5c4431df71e55ad3d2fb95b6be24d6
turrilld <turrilld@pdx.edu>
2024-10-28 21:10:16 -0700

    Part 5 backend: Submit does not work with modal.  Button does.  Will ask in class

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,17 +1,39 @@
 // Add your code here
-const form = document.getElementById("form");
-form.addEventListener("click", submit);
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", submit);
 
 const modalDump = document.getElementById("modal-dump");
 const modalClose = document.getElementById("modal-close");
+const formDump = document.getElementById("form-dump");
 modalClose.addEventListener("click", close);
 
 function submit() {
-  //   $("#modal").modal("show");
-  //   window.alert("ran");
+  formDump.innerHTML += `<p>Name: ${document.getElementById("full-name").value}</p>`;
+  formDump.innerHTML += `<p>Email: ${document.getElementById("email").value}</p>`;
+  formDump.innerHTML += `<p>Registration Status: ${
+    document.getElementById("registration-status").value
+  }</p>`;
+  if (document.getElementById("programming-languages").checked) {
+    formDump.innerHTML += `<p> Registered For: ${
+      document.getElementById("programming-languages").value
+    }</p>`;
+  }
+  if (document.getElementById("operating-systems").checked) {
+    formDump.innerHTML += `<p> Registered For: ${
+      document.getElementById("operating-systems").value
+    }</p>`;
+  }
+  if (document.getElementById("full-stack-web-dev").checked) {
+    formDump.innerHTML += `<p> Registered For: ${
+      document.getElementById("full-stack-web-dev").value
+    }</p>`;
+  }
+  formDump.innerHTML += `<p>Anything Else?: ${document.getElementById("anything-else").value}</p>`;
+
   modalDump.style.display = "block";
 }
 
 function close() {
+  formDump.innerHTML = "";
   modalDump.style.display = "none";
 }
